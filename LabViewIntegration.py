d35b51d26b32db2dc7708df684c84ce31843e268
jeyc <jeyc16@gmail.com>
2021-07-23 08:40:19 -0500

    added bringtofront killer

--- LabViewIntegration.py
+++
@@ -1,5 +0,0 @@
-# Call for macro
-
-
-if __name__ == "__main__":
-    pass
--- LabViewIntegrationKiller.py
+++ LabViewIntegrationKiller.py
@@ -3,3 +3,7 @@ try:
     os.system('taskkill /f /im LabViewIntegration.exe')
 except:
     pass
+try:
+    os.system('taskkill /f /im BringGUI2Front.exe')
+except:
+    pass
